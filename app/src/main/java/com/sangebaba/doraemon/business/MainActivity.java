740024b6b0ac092efa77b5ec57ce7f2cef975546
lixiang <lixiangers@163.com>
2016-07-20 14:23:24 +0800

    refactor

--- app/src/main/java/com/sangebaba/doraemon/business/MainActivity.java
+++ app/src/main/java/com/sangebaba/doraemon/business/MainActivity.java
@@ -34,5 +34,6 @@ public class MainActivity extends Activity {
     protected void onDestroy() {
         super.onDestroy();
         bluetoothServiceManager.onDestroy();
+        bluetoothServiceManager = null;
     }
 }
