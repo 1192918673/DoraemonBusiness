692fecd12f371941082a700d1138b8196481eff2
lixiang <lixiangers@163.com>
2016-07-20 14:22:58 +0800

    refactor

--- app/src/main/java/com/sangebaba/doraemon/business/MainActivity.java
+++ app/src/main/java/com/sangebaba/doraemon/business/MainActivity.java
@@ -1,139 +1,38 @@
 package com.sangebaba.doraemon.business;
 
 import android.app.Activity;
-import android.bluetooth.BluetoothAdapter;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.view.WindowManager;
 
-import com.sangebaba.doraemon.business.bluetooth.BluetoothChatService;
-import com.sangebaba.doraemon.business.control.Command;
-import com.sangebaba.doraemon.business.control.CommandType;
-import com.sangebaba.doraemon.business.control.Doraemon;
-import com.sangebaba.doraemon.business.util.Constant;
+import com.sangebaba.doraemon.business.bluetooth.BluetoothServiceManager;
 
 public class MainActivity extends Activity {
-    private BluetoothAdapter mBluetoothAdapter;
-    private BluetoothChatService mChatService;
-    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-
-            if (BluetoothAdapter.ACTION_STATE_CHANGED.equals(action)) {
-                int state = intent.getIntExtra(BluetoothAdapter.EXTRA_CONNECTION_STATE, -1);
-                switch (state) {
-                    case BluetoothAdapter.STATE_ON:
-                        setupChat();
-                        break;
-                    case BluetoothAdapter.STATE_OFF:
-                        if (mChatService != null) {
-                            mChatService.stop();
-                        }
-                        break;
-                }
-            }
-        }
-    };
-    private Doraemon doraemon;
-    private final Handler mHandler = new Handler() {
-        @Override
-        public void handleMessage(Message msg) {
-            switch (msg.what) {
-                case Constant.MESSAGE_STATE_CHANGE:
-                    switch (msg.arg1) {
-                        case BluetoothChatService.STATE_CONNECTED:
-                            doraemon.addCommand(new Command(CommandType.PLAY_SOUND, "已连接"));
-                            break;
-                        case BluetoothChatService.STATE_CONNECTING:
-                            doraemon.addCommand(new Command(CommandType.PLAY_SOUND, "连接中"));
-                            break;
-                        case BluetoothChatService.STATE_LISTEN:
-                        case BluetoothChatService.STATE_NONE:
-                            doraemon.addCommand(new Command(CommandType.PLAY_SOUND, "断开连接"));
-                            break;
-                    }
-                    break;
-                case Constant.MESSAGE_WRITE:
-                    byte[] writeBuf = (byte[]) msg.obj;
-                    // construct a string from the buffer
-                    String writeMessage = new String(writeBuf);
-                    break;
-                case Constant.MESSAGE_READ:
-                    byte[] readBuf = (byte[]) msg.obj;
-                    // construct a string from the valid bytes in the buffer
-                    String readMessage = new String(readBuf, 0, msg.arg1);
-                    doraemon.addCommand(new Command(CommandType.PLAY_SOUND, readMessage));
-                    break;
-                case Constant.MESSAGE_DEVICE_NAME:
-                    // save the connected device's name
-                    break;
-                case Constant.MESSAGE_TOAST:
-                    break;
-            }
-        }
-    };
+    private BluetoothServiceManager bluetoothServiceManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.activity_main);
-        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        IntentFilter filter = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
-        this.registerReceiver(mReceiver, filter);
-        doraemon = Doraemon.getInstance(getApplicationContext());
+        bluetoothServiceManager = BluetoothServiceManager.getInstance(getApplicationContext());
+        bluetoothServiceManager.onCreate();
     }
 
     @Override
     public void onStart() {
         super.onStart();
-        // If BT is not on, request that it be enabled.
-        // setupChat() will then be called during onActivityResult
-        if (!mBluetoothAdapter.isEnabled()) {
-            mBluetoothAdapter.enable();
-            // Otherwise, setup the chat session
-        } else if (mChatService == null) {
-            setupChat();
-        }
+        bluetoothServiceManager.onStart();
     }
 
     @Override
     public void onResume() {
         super.onResume();
-
-        // Performing this check in onResume() covers the case in which BT was
-        // not enabled during onStart(), so we were paused to enable it...
-        // onResume() will be called when ACTION_REQUEST_ENABLE activity returns.
-        if (mChatService != null) {
-            // Only if the state is STATE_NONE, do we know that we haven't started already
-            if (mChatService.getState() == BluetoothChatService.STATE_NONE) {
-                // Start the Bluetooth chat services
-                mChatService.start();
-            }
-        }
-    }
-
-    private void setupChat() {
-        if (mChatService == null)
-            mChatService = new BluetoothChatService(getApplicationContext(), mHandler);
-        if (mChatService.getState() == BluetoothChatService.STATE_NONE) {
-            // Start the Bluetooth chat services
-            mChatService.start();
-        }
+        bluetoothServiceManager.onResume();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        this.unregisterReceiver(mReceiver);
-        if (mChatService != null) {
-            mChatService.stop();
-        }
+        bluetoothServiceManager.onDestroy();
     }
 }
---
+++ app/src/main/java/com/sangebaba/doraemon/business/bluetooth/BluetoothServiceManager.java
@@ -0,0 +1,136 @@
+package com.sangebaba.doraemon.business.bluetooth;
+
+import android.bluetooth.BluetoothAdapter;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Handler;
+import android.os.Message;
+
+import com.sangebaba.doraemon.business.control.Command;
+import com.sangebaba.doraemon.business.control.CommandType;
+import com.sangebaba.doraemon.business.control.Doraemon;
+import com.sangebaba.doraemon.business.util.Constant;
+
+public class BluetoothServiceManager {
+    private static volatile BluetoothServiceManager instance;
+    private BluetoothAdapter mBluetoothAdapter;
+    private Doraemon doraemon;
+    private final Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case Constant.MESSAGE_STATE_CHANGE:
+                    switch (msg.arg1) {
+                        case BluetoothChatService.STATE_CONNECTED:
+                            doraemon.addCommand(new Command(CommandType.PLAY_SOUND, "已连接"));
+                            break;
+                        case BluetoothChatService.STATE_CONNECTING:
+                            doraemon.addCommand(new Command(CommandType.PLAY_SOUND, "连接中"));
+                            break;
+                        case BluetoothChatService.STATE_LISTEN:
+                        case BluetoothChatService.STATE_NONE:
+                            doraemon.addCommand(new Command(CommandType.PLAY_SOUND, "断开连接"));
+                            break;
+                    }
+                    break;
+                case Constant.MESSAGE_WRITE:
+                    byte[] writeBuf = (byte[]) msg.obj;
+                    // construct a string from the buffer
+                    String writeMessage = new String(writeBuf);
+                    break;
+                case Constant.MESSAGE_READ:
+                    byte[] readBuf = (byte[]) msg.obj;
+                    // construct a string from the valid bytes in the buffer
+                    String readMessage = new String(readBuf, 0, msg.arg1);
+                    doraemon.addCommand(new Command(CommandType.PLAY_SOUND, readMessage));
+                    break;
+                case Constant.MESSAGE_DEVICE_NAME:
+                    // save the connected device's name
+                    break;
+                case Constant.MESSAGE_TOAST:
+                    break;
+            }
+        }
+    };
+    private Context context;
+    private BluetoothChatService mChatService;
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+
+            if (BluetoothAdapter.ACTION_STATE_CHANGED.equals(action)) {
+                int state = intent.getIntExtra(BluetoothAdapter.EXTRA_CONNECTION_STATE, -1);
+                switch (state) {
+                    case BluetoothAdapter.STATE_ON:
+                        setupChat();
+                        break;
+                    case BluetoothAdapter.STATE_OFF:
+                        if (mChatService != null) {
+                            mChatService.stop();
+                        }
+                        break;
+                }
+            }
+        }
+    };
+
+    private BluetoothServiceManager(Context context) {
+        this.context = context;
+    }
+
+    public static BluetoothServiceManager getInstance(Context context) {
+        if (instance == null) {
+            synchronized (BluetoothServiceManager.class) {
+                if (instance == null) {
+                    instance = new BluetoothServiceManager(context);
+                }
+            }
+        }
+        return instance;
+    }
+
+    public void onCreate() {
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        IntentFilter filter = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+        context.registerReceiver(mReceiver, filter);
+        doraemon = Doraemon.getInstance(context);
+    }
+
+    public void onStart() {
+        if (!mBluetoothAdapter.isEnabled()) {
+            mBluetoothAdapter.enable();
+            // Otherwise, setup the chat session
+        } else if (mChatService == null) {
+            setupChat();
+        }
+    }
+
+    public void onResume() {
+        if (mChatService != null) {
+            // Only if the state is STATE_NONE, do we know that we haven't started already
+            if (mChatService.getState() == BluetoothChatService.STATE_NONE) {
+                // Start the Bluetooth chat services
+                mChatService.start();
+            }
+        }
+    }
+
+    public void onDestroy() {
+        context.unregisterReceiver(mReceiver);
+        if (mChatService != null) {
+            mChatService.stop();
+        }
+    }
+
+    private void setupChat() {
+        if (mChatService == null)
+            mChatService = new BluetoothChatService(context, mHandler);
+        if (mChatService.getState() == BluetoothChatService.STATE_NONE) {
+            // Start the Bluetooth chat services
+            mChatService.start();
+        }
+    }
+}
