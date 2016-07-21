64f2c858b69479befed68ae7171eae42d115d6cf
lixiang <lixiangers@163.com>
2016-07-21 11:42:14 +0800

    添加 开机启动

---
+++ app/src/main/java/com/geeknewbee/doraemon/Broadcast/BootReceiver.java
@@ -0,0 +1,16 @@
+package com.geeknewbee.doraemon.Broadcast;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import com.geeknewbee.doraemon.MainActivity;
+
+public final class BootReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent arg1) {
+        Intent intent = new Intent(context, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(intent);
+    }
+}
