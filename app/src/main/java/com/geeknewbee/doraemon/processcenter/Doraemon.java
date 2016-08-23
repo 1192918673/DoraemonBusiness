ed4f8d208a7a4fc300ae859491b34d311e3ec924
zhangzhanqian <zhanqian.zhang@newbeeair.com>
2016-08-23 15:47:45 +0800

    Mouth Task完成后开启asr关闭edd

--- app/src/main/java/com/geeknewbee/doraemon/processcenter/Doraemon.java
+++ app/src/main/java/com/geeknewbee/doraemon/processcenter/Doraemon.java
@@ -161,7 +161,7 @@ public class Doraemon implements IEar.ASRListener, IEye.AFRListener, IMessageRec
     @Subscribe(threadMode = ThreadMode.MAIN)
     public void onTTSComplete(TTSCompleteEvent event) {
         //完成后开启语音监听,当在有播放媒体的时候不需要的时候，这里需要处理
-        startASR();
+        switchListener(SoundMonitorType.ASR);
     }
 
     /**
@@ -172,7 +172,7 @@ public class Doraemon implements IEar.ASRListener, IEye.AFRListener, IMessageRec
     @Subscribe(threadMode = ThreadMode.MAIN)
     public void onPlayMusicComplete(MusicCompleteEvent event) {
         //完成后开启语音监听
-        startASR();
+        switchListener(SoundMonitorType.ASR);
     }
 
     @Subscribe(threadMode = ThreadMode.MAIN)
