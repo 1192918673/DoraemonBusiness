98676f720dc49c498c898b4ca833e24e6d6c1792
lixiang <lixiangers@163.com>
2016-09-19 09:42:13 +0800

    更新开始结束字符

--- app/src/main/java/com/geeknewbee/doraemon/constants/Constants.java
+++ app/src/main/java/com/geeknewbee/doraemon/constants/Constants.java
@@ -37,8 +37,10 @@ public class Constants {
     public static final String LISTENNING_GIF = "eyegif_fa_dai";
 
     public static final String STOP_FLAG = "停";
-    public static final String COMMAND_ROBOT_PREFIX = "DRC";
-    public static final String COMMAND_ROBOT_SUFFIX = "DRC_SUFFIX";
+    //    public static final String COMMAND_ROBOT_PREFIX = "DRC";
+//    public static final String COMMAND_ROBOT_SUFFIX = "DRC_SUFFIX";
+    public static final String COMMAND_ROBOT_PREFIX = String.valueOf(0x02);
+    public static final String COMMAND_ROBOT_SUFFIX = String.valueOf(0x03);
     public static final String MOJI_WEATHER_API_TOKEN = "c4adee396b1260222696";
     //墨迹天气
     public static final String MOJI_WEATHER_API_PWD = "mojisgbb20151207";
