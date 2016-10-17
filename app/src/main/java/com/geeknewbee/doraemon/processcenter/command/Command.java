ba1fff430659570cb06f0bdd10c6570e12e1cf1b
lixiang <lixiangers@163.com>
2016-10-17 10:06:42 +0800

    Command 添加ＩＤ

--- app/src/main/java/com/geeknewbee/doraemon/processcenter/command/Command.java
+++ app/src/main/java/com/geeknewbee/doraemon/processcenter/command/Command.java
@@ -6,18 +6,18 @@ import com.geeknewbee.doraemon.constants.Constants;
  * Brain执行的命令
  */
 public class Command {
-
+    private long id;
     private CommandType type;
     private String content;
 
     public Command(CommandType type, String content) {
         this.type = type;
         this.content = content;
+        id = System.currentTimeMillis();
     }
 
     public Command(CommandType type) {
-        this.type = type;
-        this.content = Constants.EMPTY_STRING;
+        this(type, Constants.EMPTY_STRING);
     }
 
     public CommandType getType() {
@@ -26,6 +26,10 @@ public class Command {
 
     public String getContent() {
         return content;
+    }
+
+    public long getId() {
+        return id;
     }
 
     @Override
